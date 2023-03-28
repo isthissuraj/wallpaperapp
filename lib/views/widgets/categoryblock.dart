730ca306d7e020944f565d4df1a8501f5eae6789
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-03-29 00:05:22 +0530

    category block completed in ui

---
+++ lib/views/widgets/categoryblock.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CategoryBlock extends StatelessWidget {
+  const CategoryBlock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.network(
+                height: 50,
+                width: 100,
+                fit: BoxFit.cover,
+                "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          ),
+          Container(
+            height: 50,
+            width: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12), color: Colors.black26),
+          ),
+          Positioned(
+              left: 35,
+              top: 15,
+              child: Text(
+                "Cars",
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+              ))
+        ],
+      ),
+    );
+  }
+}
