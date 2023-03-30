717f5741d22e59d34806db3e6a2d5e2ce7f8cb48
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-03-31 01:11:10 +0530

    ApiOpetrations function created

--- lib/controller/apiOperations.dart
+++ lib/controller/apiOperations.dart
@@ -1,3 +1,10 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
-class ApiOperations {}
+class ApiOperations {
+  getTrendingWallpapers() async {
+    await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
+      "Authorization":
+          "2e7mdYOSgtT8kwaYmff5jqPrkgpTAKXkZfn6t0H1ns2nxdIm887fJ7aP"
+    }).then((value) => print(value.body));
+  }
+}
