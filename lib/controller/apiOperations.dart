0d267190454a634980a13287ddfd74bd3ce1c9a3
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-03-31 01:30:28 +0530

    api code created

--- lib/controller/apiOperations.dart
+++ lib/controller/apiOperations.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class ApiOperations {
@@ -5,6 +7,13 @@ class ApiOperations {
     await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
       "Authorization":
           "2e7mdYOSgtT8kwaYmff5jqPrkgpTAKXkZfn6t0H1ns2nxdIm887fJ7aP"
-    }).then((value) => print(value.body));
+    }).then((value) {
+      Map<String, dynamic> jsonData = jsonDecode(value.body);
+      List photos = jsonData["photos"];
+      photos.forEach((element) {
+        Map<String, dynamic> src = element["src"];
+        print(src["portrate"]);
+      });
+    });
   }
 }
