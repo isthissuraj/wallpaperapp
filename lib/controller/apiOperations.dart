ae21ee2a78c2e38a27e29821b37a826f8bc10637
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-04-07 22:09:49 +0530

    search wallpaper function added

--- lib/controller/apiOperations.dart
+++ lib/controller/apiOperations.dart
@@ -5,6 +5,7 @@ import 'package:wallpaperapp/model/photosModel.dart';
 
 class ApiOperations {
   static List<PhotosModel> trendingWallpapers = [];
+  static List<PhotosModel> searchReasultList = [];
 
   static Future<List<PhotosModel>> getTrendingWallpapers() async {
     await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
@@ -19,6 +20,25 @@ class ApiOperations {
     });
 
     return trendingWallpapers;
+  }
+
+  searchWallpapers(String query) async {
+    await http.get(
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=30&page=1"),
+        headers: {
+          "Authorization":
+              "2e7mdYOSgtT8kwaYmff5jqPrkgpTAKXkZfn6t0H1ns2nxdIm887fJ7aP"
+        }).then((value) {
+      Map<String, dynamic> jsonData = jsonDecode(value.body);
+      List photos = jsonData["photos"];
+      photos.forEach((element) {
+        searchReasultList.clear();
+        searchReasultList.add(PhotosModel.fromApiToApp(element));
+      });
+    });
+
+    return searchReasultList;
   }
 }
 
