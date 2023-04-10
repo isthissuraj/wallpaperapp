b8c4eeac038d28b6ec5a72800dfac91b44af4cbc
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-04-10 23:09:01 +0530

    search wallpaper function added

--- lib/controller/apiOperations.dart
+++ lib/controller/apiOperations.dart
@@ -7,6 +7,7 @@ class ApiOperations {
   static List<PhotosModel> trendingWallpapers = [];
   static List<PhotosModel> searchReasultList = [];
 
+// trending wallpaper method
   static Future<List<PhotosModel>> getTrendingWallpapers() async {
     await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
       "Authorization":
@@ -22,6 +23,7 @@ class ApiOperations {
     return trendingWallpapers;
   }
 
+// serach wallpaper method
   searchWallpapers(String query) async {
     await http.get(
         Uri.parse(
