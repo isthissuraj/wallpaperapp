23c8b2b6393af09a3ebc81688fd26a63430d5839
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-04-15 00:11:34 +0530

    category part done

--- lib/views/screens/categoryscr.dart
+++ lib/views/screens/categoryscr.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:wallpaperapp/controller/apiOperations.dart';
 import 'package:wallpaperapp/model/photosModel.dart';
--- lib/views/widgets/categoryblock.dart
+++ lib/views/widgets/categoryblock.dart
@@ -23,10 +23,7 @@ class CategoryBlock extends StatelessWidget {
             ClipRRect(
               borderRadius: BorderRadius.circular(12),
               child: Image.network(
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.cover,
-                  "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                  height: 50, width: 100, fit: BoxFit.cover, categoryImgSrc),
             ),
             Container(
               height: 50,
@@ -35,12 +32,12 @@ class CategoryBlock extends StatelessWidget {
                   borderRadius: BorderRadius.circular(12),
                   color: Colors.black26),
             ),
-            const Positioned(
+            Positioned(
                 left: 35,
                 top: 15,
                 child: Text(
-                  "Cars",
-                  style: TextStyle(
+                  categoryName,
+                  style: const TextStyle(
                       fontWeight: FontWeight.w600, color: Colors.white),
                 ))
           ],
