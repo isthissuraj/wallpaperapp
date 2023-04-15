2f8ec69d541bc1c47cdf592c8d03ead367d4b05c
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-04-16 04:27:59 +0530

    wallpaper download integrated

--- lib/views/screens/fullScreen.dart
+++ lib/views/screens/fullScreen.dart
@@ -1,22 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:open_file/open_file.dart';
 
 // ignore: must_be_immutable
 class FullScreen extends StatelessWidget {
   String imgUrl;
   FullScreen({super.key, required this.imgUrl});
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  Future<void> setWallpaperFromFile(
+      String wallpaperUrl, BuildContext context) async {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Downloading start..")));
+    try {
+      var imageId = await ImageDownloader.downloadImage(wallpaperUrl);
+      if (imageId == null) {
+        return;
+      }
+      var fileName = await ImageDownloader.findName(imageId);
+      var path = await ImageDownloader.findPath(imageId);
+      var size = await ImageDownloader.findByteSize(imageId);
+      var mimeType = await ImageDownloader.findMimeType(imageId);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(" Downloaded Sucessfully"),
+        action: SnackBarAction(
+          onPressed: () {
+            OpenFile.open(path);
+          },
+          label: "Open",
+        ),
+      ));
+      print("IMAGE DOWNLOADED");
+    } on PlatformException catch (error) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Error Occured $error")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
+        floatingActionButton: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.orangeAccent),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+            ),
+          ),
+          onPressed: () async {
+            await setWallpaperFromFile(imgUrl, context);
+          },
+          child: Text("Download wallpaper"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         body: Hero(
-      tag: imgUrl,
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage(imgUrl), fit: BoxFit.cover)),
-      ),
-    ));
+          tag: imgUrl,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(imgUrl), fit: BoxFit.cover)),
+          ),
+        ));
   }
 }
